07c301d636cd25e7ad0b78ce15fad6392426b767
Brett Zamir <brettz9@yahoo.com>
2015-06-25 19:06:04 -0700

    Further work on database enumeration; getJSON dependency

---
+++ index.js
@@ -0,0 +1,32 @@
+/*globals getJSON, jml*/
+(function () {'use strict';
+
+document.title = "Sacred Writings Browser";
+
+getJSON('databases.json', function (json) {
+    jml(
+        'div',
+        {style: 'text-align:center'},
+        json.map(function (db, i) {
+            return ['div', [
+                (i > 0 ? ['br', 'br', 'br'] : ''),
+                ['div', [db.directions]],
+                ['br'],
+                ['select', {'class': 'file', $dataset: {name: db.name}, $on: {change: function (e) {
+                    // Submit
+                    alert(e.target.dataset.name);
+                }}}, 
+                    db.files.map(function (file) {
+                        return ['option', {$dataset: {file: file.file}}, [file.name]];
+                    })
+                ],
+                ['p', [
+                    ['input', {type: 'button', value: "Go"}]
+                ]]
+            ]];
+        }),
+        document.body
+    );
+});
+
+}());
