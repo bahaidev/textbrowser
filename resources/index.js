29690e666c04af3a080d33b2c06a7e901f834ae5
Brett Zamir <brettz9@yahoo.com>
2015-07-07 21:42:05 -0700

    todo: try relative refs when ready

--- resources/index.js
+++ resources/index.js
@@ -13,7 +13,7 @@ Todos (higher priority)
 1. Choose clearer naming/structure for locale table/field keys
     1. Consider tablealias and default to table or something (as fieldalias defaults to fieldname)
     1. Consider using prefer_alias for field alias use and optionally show both?
-1. Once updated, add and make use of updated json-refs to make single resolveRef call.
+1. Once updated, add and make use of updated json-refs to make single resolveRef call and try relative refs.
 
 
 1. Get the automated fields listed in drop-down menus (See README todos)
