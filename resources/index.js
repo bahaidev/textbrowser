7c343d2a1b4010e6ef2ba419898f25bd66bd522e
Brett Zamir <brettz9@yahoo.com>
2015-07-08 12:06:03 -0700

    todo

--- resources/index.js
+++ resources/index.js
@@ -12,6 +12,7 @@ Todos (higher priority)
 1. Choose clearer naming/structure for locale table/field keys
     1. Consider tablealias and default to table or something (as fieldalias defaults to fieldname); aliased heading (also used as the title of the page)
     1. Consider using prefer_alias for field alias use and optionally show both?
+1. Remove "numbers only" strings (including from locale files?) if allowing for aliased searches (e.g., "Gen")
 1. Once updated, add and make use of updated json-refs to make single resolveRef call and try relative refs.
 1. Review code for readability
 
