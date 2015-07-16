579852cecedb76bfc5e2591ac63e62403b38273f
Brett Zamir <brettz9@yahoo.com>
2015-07-16 00:35:21 -0700

    todo, locale grouping

--- resources/index.js
+++ resources/index.js
@@ -21,6 +21,7 @@ Todos (higher priority)
     1. Consider using `prefer_alias` for field alias use and optionally show both if not given (e.g., for Bible books with `prefer_alias`, show only the pull-down of books whereas with the Qur'an (where Surah numbers are more commonly used) link a pull-down of Surah names to a textbox allowing numbers)?
     1. Remove locale info for "numbers only" string key (including from locale files?) if allowing for aliased searches (e.g., "Gen")
 1. Options to have range of context and range for highlighting (with own styles) and anchoring
+1. Add content language(s) multiple select option to always browse for those in the desired locale(s)
 1. Schema-aware and metadata-aware column sorting options (e.g., sort by order and ASC/DESC) with user customizability (i.e., presorting along with dynamic client-side after-load sorting, with or without search filtering)
 1. Get the automated fields listed in drop-down menus; also new overlay type (See README todos)
 
