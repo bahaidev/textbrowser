13a3a57a96f2effffb7409f46a9cc066d2f41390
Brett Zamir <brettz9@yahoo.com>
2015-10-25 14:16:04 -0700

    move todos

--- resources/index.js
+++ resources/index.js
@@ -1,43 +1,6 @@
 /*global formSerialize, JsonRefs, IMF, getJSON, jml, URLSearchParams, window, alert, document, location*/
-/*jslint vars:true, regexp:true, todo:true*/
 /*eslint max-len: 0, require-jsdoc: 0, no-alert: 0, no-warning-comments: 0, no-magic-numbers: 0, no-extra-parens: 0, lines-around-comment: 0*/
 /* exported TextBrowser*/
-
-/*
-Todos (higher priority)
-
-1. WAITING (version update): Add and make use of updated json-refs to make single `resolveRemoteRef` call (or whatever the new API may become) and try new relative refs feature.
-
-1. Change to utilize history.pushState https://developer.mozilla.org/en-US/docs/Web/API/History_API
-1. Cbeck whether URLSearchParams.toString is still working
-
-1. Fix code to work with table-specific/field-specific locale data that has been moved to files.json (and languages.json) and metadata files for modularity; then avoid unchecking when clicking button re: matching current locale if `fieldvalue` is present (i.e., replace `hasFieldvalue` functionality)
-    1. Update README and metadata.jsonschema (or table.jsonschema?) to reflect new changes
-1. Rename localization strings, esp. auto-field ones; consider making some reusable, pointing to scripts, or how else to designate auto-field plugins?
-    1. Update README and files.jsonschema to reflect new changes
-1. Choose clearer naming/structure for locale table/field keys
-    1. Consider `tablealias` and default to `table` or something (as `fieldalias` defaults to `fieldname`); aliased heading (also used as the title of the page)
-1. Update schemas to reflect new localization-related file changes (languages.json, files.json, metadata files) and make one for site-sample.json (and move to bahaiwritings as site.json)
-1. Review code for readability, refactoring opportunities
-
-1. Aliases
-    1. Consider using `prefer_alias` for field alias use and optionally show both if not given (e.g., for Bible books with `prefer_alias`, show only the pull-down of books whereas with the Qur'an (where Surah numbers are more commonly used) link a pull-down of Surah names to a textbox allowing numbers)?
-    1. Remove locale info for "numbers only" string key (including from locale files?) if allowing for aliased searches (e.g., "Gen")
-1. Build library (for browser or Node) to utilize site.json file to add site-wide navigation bar headers, breadcrumbs, link rel=next/prev/contents/etc., sitemap, and page title (supplied argument of the current page)? Also about text and removecookies.
-1. Code to populate locale files with missing localization strings and report the missing ones (and sort as such in assistant file); put assistant localization keys in own file?
-1. Node.js delivery of HTML content by same URL so third parties can consume without JavaScript
-1. Incorporate speech synthesis from http://bahai.works/MediaWiki:Common.js
-1. Options to have range of context and range for highlighting (with own styles) and anchoring
-1. CORS and HTTPQuery headers?
-1. Add content language(s) multiple select option to always browse for those in the desired locale(s); utilize "localization-strings"/<code>/languages/<code> in language.json
-1. Schema-aware and metadata-aware column sorting options (e.g., sort by order and ASC/DESC) with user customizability (i.e., presorting along with dynamic client-side after-load sorting, with or without search filtering; use "search" in locale to add this filtering to UI)
-1. Get the automated fields listed in drop-down menus; also new overlay type (See README todos)
-
-Todos (for browse9.php equivalent)
-1. Handle defaults for empty boxes if not already
-1. Test all locales and works and combos
-1. Utilize `prefer_alias`
-*/
 
 var TextBrowser = (function () {'use strict';
 
