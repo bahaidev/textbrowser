441297be30e00ea24a03c4cb58821eb7e0412791
Brett Zamir <brettz9@yahoo.com>
2017-05-30 10:15:33 +0800

    Get resultsDisplay page showing all data for now

--- resources/resultsDisplay.js
+++ resources/resultsDisplay.js
@@ -1,4 +1,4 @@
-/* globals TextBrowser, Templates */
+/* globals TextBrowser, Templates, JsonRefs, jml */
 TextBrowser.prototype.resultsDisplay = function resultsDisplay ({
     l, lang, localeFromFileData, fallbackLanguages, $p
 }) {
@@ -8,6 +8,16 @@ TextBrowser.prototype.resultsDisplay = function resultsDisplay ({
         console.log('pluginKeys', pluginKeys);
         console.log('pluginFieldMappings', pluginFieldMappings);
         console.log('pluginObjects', pluginObjects);
+
+        // Todo: Needs to actually take params into account!
+        JsonRefs.resolveRefs(fileData.file).then(({resolved: {data: tableData}}) => {
+            jml('table', {border: '1'}, tableData.map((tr) => ['tr',
+                tr.map((td) => ['td',
+                    // [td] // Todo: For non-escaped!!!!
+                    {innerHTML: td}
+                ])
+            ]), document.body);
+        });
 
         document.title = l({
             key: 'browserfile-resultsdisplay',
