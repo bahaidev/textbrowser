62c3c7f2c2a8a97a883495fb54ed1e0106bfcbda
Brett Zamir <brettz9@yahoo.com>
2024-09-21 19:44:39 +0800

    chore: prevent indexeddbshim updates by ncu

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -2,5 +2,8 @@
 
 module.exports = {
   reject: [
+    // Versions past 12 have some apparent node-sqlite3 issues,
+    //   interacting with rollup, so we can't run server script
+    'indexeddbshim'
   ]
 };
